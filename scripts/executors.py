5758baabf726a5b60d7e021bf037019b8eb4eb92
Betfsh <bettymegd@gmail.com>
2024-07-27 23:04:57 +0300

    importing libraries

--- scripts/executors.py
+++ scripts/executors.py
@@ -1,4 +1,4 @@
 from langchain.agents import AgentType, initialize_agent
 from langchain.schema import SystemMessage
-#import logging
+import logging
 #from tools import generate_image, change_image_size, insert_text_on_image, combine_images_to_create_frame
