791324db72839b0a8d3576f0e517f181dd63d4ea
Betfsh <bettymegd@gmail.com>
2024-07-28 21:44:03 +0300

    system message

--- scripts/executors.py
+++ scripts/executors.py
@@ -3,3 +3,8 @@ from langchain.schema import SystemMessage
 import logging
 from tools import generate_image, change_image_size, insert_text_on_image, combine_images_to_create_frame
 insert_text_on_image, combine_images_to_create_frame 
+
+logging.basicConfig(level=logging.INFO)
+
+with open("system_message.txt", "r") as file:
+    system_message = file.read()
